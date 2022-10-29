5fe5257706d78ac4d525e134c383fff97079aeca
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-10-28 21:26:44 -0400

    Update lang.js

--- lang.js
+++ lang.js
@@ -14,3 +14,17 @@ function setCookie(cname, cvalue, exdays) {
   let expires = "expires="+d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
