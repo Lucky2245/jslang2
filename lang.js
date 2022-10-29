b8a375b7d8dcba71639c5319eac3386c934480ea
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-10-28 21:24:00 -0400

    Update lang.js

--- lang.js
+++ lang.js
@@ -8,22 +8,23 @@ function cookiesEnabled(){
         return false;
     }
 }
-function setCookie(cname, cvalue, exdays){
+function setCookie(cname, cvalue, exdays) {
   const d = new Date();
-  d.setTime(d.getTime *+ (exdays*24*60*60));
-  let expires = "expires" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue" + ";" + expires + ";patth=/";
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
-function getCookie(cname){
+function getCookie(cname) {
   let name = cname + "=";
-  decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  let c = ca[i];
-  while(c.charAt(0) == '0'){
-    c = c.substring(1);
-  }
-  if(c.indexof(name) == 0){
-    return c.substring(name.length, c.length);
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
   }
   return "";
 }
